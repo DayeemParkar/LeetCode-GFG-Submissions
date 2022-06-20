ed89a3f8e35283200b872fb3576f330d03eae131
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2022-06-20 23:00:12 +0530

    Time: 371 ms (30.23%), Space: 16.3 MB (52.56%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        l = len(words)
+        trie = {}
+        words.sort(key = len, reverse=1)
+        encoding = 0
+        for word in words:
+            word = word[::-1]
+            cur = trie
+            suffix_in_trie = 1
+            for c in word:
+                if c in cur:
+                    cur = cur[c]
+                else:
+                    cur[c] = {}
+                    cur = cur[c]
+                    suffix_in_trie = 0
+                    
+            if not suffix_in_trie:
+                encoding += len(word) + 1
+                
+        return encoding
